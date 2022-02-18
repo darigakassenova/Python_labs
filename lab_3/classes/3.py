9e7352b4052afb5904268e9b98af6b26feb44597
Dariga <98644753+darigakassenova@users.noreply.github.com>
2022-02-18 23:30:14 +0600

    Update 3.py

--- lab_3/classes/3.py
+++ lab_3/classes/3.py
@@ -1,11 +1,1 @@
-class Shape():
-    def _init_(self,lenght,width):
-        self.lenght=int(input())
-        self.width=int(input())
-    def area(self):
-        print(self.lenght*self.width)
-class Rectangle(Shape):
-    def _init_(self,lenght,width):
-        Shape._init_(self,lenght,width)
-x = Rectangle(Shape)
-x.area()
+
